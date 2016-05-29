3796b9136b4e1fe94c3aa710ecc69954daed0dd8
Tomasz Belina <tomasz.belina@qualent.eu>
2016-05-30 00:18:53 +0200

    aktualizacja

--- learning-CRM/app/users/users.directives.js
+++ learning-CRM/app/users/users.directives.js
@@ -5,14 +5,15 @@
 
       return {
           scope: {
-              member: '='
+              member: '=',
+              devs: '='
           },
           restrict: 'AEC',
           //template: '<div class="well text-center bc-style"><img ng-src="{{ member.avatar }}" alt=""><h4>{{ member.name }}</h4><h6>{{ member.position }}</h6></div>'
           //templateUrl: 'member-simple-bc.html'
           templateUrl: function (elem, attr) {
 
-              attr.type = attr.type||'simple';
+              attr.type = attr.type||'dev';
 
               return 'app/users/member-'+attr.type+'-bc.html';
 
