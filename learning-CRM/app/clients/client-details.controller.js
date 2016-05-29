f4396f3772a54d19b2f9a68edb105618908a057a
Tomasz Belina <tomasz.belina@qualent.eu>
2016-05-29 13:10:00 +0200

    Dodaj walidacje

--- learning-CRM/app/clients/client-details.controller.js
+++ learning-CRM/app/clients/client-details.controller.js
@@ -1,25 +1,29 @@
 (function(){
 
     angular.module('crmApp')
-    .controller('ClientDetailsCtrl', ['$scope', '$routeParams', '$location','clientsService', 'usersService', 'sectorsService', 
-        function($scope, $routeParams, $location, clientsService, usersService, sectorsService){
+    .controller('ClientDetailsCtrl', ['$scope', '$routeParams', '$timeout', '$location','clientsService', 'usersService', 'sectorsService', 
+        function($scope, $routeParams, $timeout, $location, clientsService, usersService, sectorsService){
             $scope.client = {};//{ "id": 1, "company_name": "Lorem ipsum dolor", "contact_name": "Michalina Kwiatkowska", "contact_phone": "53 790 92 21", "contact_email": "MichalinaKwiatkowska@dayrep.com",             "account_manager_id": 1, "notes": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", "sector_id": 5 };
             $scope.users = []; //[ { "id": 1, "name": "Konstantyn Kowalski" },             { "id": 2, "name": "Łukasz Pawłowski" }];
             $scope.sectors = [];
-            $scope.clientNotFound = false;
 
-            clientsService.getClient(
-                $routeParams.clientId,
-                function (data) {
-                   $scope.client = data[0];   
-                   console.log($scope.client);              
-                },
-                function (data, status) {
-                    if(404 == status){
-                        $scope.clientNotFound = true;
+            $scope.clientNotFound = false;
+            $scope.showSaveClientFormMsg = false;
+
+            if('new' != $routeParams.clientId){
+                clientsService.getClient(
+                    $routeParams.clientId,
+                    function (data) {
+                        $scope.client = data[0];   
+                        console.log($scope.client);              
+                    },
+                    function (data, status) {
+                        if(404 == status){
+                            $scope.clientNotFound = true;
+                        }
                     }
-                }
-            );
+                );
+            }
             sectorsService.getSectors(function(sectors){
                 $scope.sectors = sectors;
             });
@@ -30,14 +34,21 @@
 
             $scope.saveClientData = function(){
 
+                if($scope.clientForm.$invalid) return;
+
                 if('new' == $routeParams.clientId){
                     clientsService.saveNewClient($scope.client, function (clientId) {
-                        console.log(clientId);
                         $location.path('/clients/'+clientId);
                     });
                 }else{
                     clientsService.updateClient($scope.client.id, $scope.client, function (data) {
-                    
+                        $scope.showSaveClientFormMsg = true;
+
+                    $timeout(function () {
+
+                        $scope.showSaveClientFormMsg = false;
+
+                    }, 2000);
                     });
                 }
 
